15c84c21dc9a03d9fb717b9d34c8fbebdc2298dd
davidecravedi168-beep <davide.cravedi168@gmail.com>
2026-09-11 23:15:33 +0200

    fix(ui): restore navigation and portraits runtime v3.5

--- player-photos.js
+++ player-photos.js
@@ -1,32 +1,146 @@
 // Generated from public Wikimedia/Wikidata sources.
 window.__TEP_PLAYER_PHOTOS__=Object.freeze({
-  "alevtina ibragimova": "https://thumb.wikimedia.org/wikipedia/commons/thumb/5/5c/Alevtina_Ibragimova_%282026_US_Open%29_02_%28cropped%29.jpg/330px-Alevtina_Ibragimova_%282026_US_Open%29_02_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "alexander zverev": "https://thumb.wikimedia.org/wikipedia/commons/thumb/2/2f/Alexander_Zverev.jpg/330px-Alexander_Zverev.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "alfie hewett": "https://thumb.wikimedia.org/wikipedia/commons/thumb/4/47/Alfie_Hewett_%2835580720080%29.jpg/330px-Alfie_Hewett_%2835580720080%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "anna blinkova": "https://thumb.wikimedia.org/wikipedia/commons/thumb/8/86/Day_6_-_Transilvania_Open_Anna_Blinkova_%28cropped%29.jpg/330px-Day_6_-_Transilvania_Open_Anna_Blinkova_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "anna pushkareva": "https://thumb.wikimedia.org/wikipedia/commons/thumb/7/76/Anna_Pushkareva_25May24_0306.jpg/330px-Anna_Pushkareva_25May24_0306.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "aryna sabalenka": "https://thumb.wikimedia.org/wikipedia/commons/thumb/5/57/Aryna_Sabalenka_Miami_Open_Final.jpg/330px-Aryna_Sabalenka_Miami_Open_Final.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "ben shelton": "https://thumb.wikimedia.org/wikipedia/commons/thumb/1/1b/Ben_Shelton%2C_media_conference%2C_Swiss_Indoors_Basel_2025_%28cropped%29.jpg/330px-Ben_Shelton%2C_media_conference%2C_Swiss_Indoors_Basel_2025_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "claire liu": "https://thumb.wikimedia.org/wikipedia/commons/thumb/1/18/Claire_Liu_%282026_US_Open%29_01_%28cropped%29.jpg/330px-Claire_Liu_%282026_US_Open%29_01_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "diede de groot": "https://thumb.wikimedia.org/wikipedia/commons/thumb/1/18/25th_Laureus_World_Sports_Awards_-_Red_Carpet_-_Diede_de_Groot_-_240422_184135_%28cropped%29.jpg/330px-25th_Laureus_World_Sports_Awards_-_Red_Carpet_-_Diede_de_Groot_-_240422_184135_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "elena rybakina": "https://thumb.wikimedia.org/wikipedia/commons/thumb/d/d3/Elena_Rybakina_%282025_DC_Open%29_11_%28cropped%29.jpg/330px-Elena_Rybakina_%282025_DC_Open%29_11_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "elsa jacquemot": "https://thumb.wikimedia.org/wikipedia/commons/thumb/8/8d/Elsa_Jacquemot_%282023_US_Open%29_09_%28cropped%29.jpg/330px-Elsa_Jacquemot_%282023_US_Open%29_09_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "facundo diaz acosta": "https://thumb.wikimedia.org/wikipedia/commons/thumb/6/6b/Road_2_Australia_-_Navone_V_Diaz_Acosta_semifinal_-_BugWarp_139_%28cropped%29.jpg/330px-Road_2_Australia_-_Navone_V_Diaz_Acosta_semifinal_-_BugWarp_139_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "fiona ferro": "https://thumb.wikimedia.org/wikipedia/commons/thumb/0/06/Ferro_RG19_%2810%29_%2848199369072%29.jpg/330px-Ferro_RG19_%2810%29_%2848199369072%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "frances tiafoe": "https://thumb.wikimedia.org/wikipedia/commons/thumb/a/a3/FrancesTiafo.jpg/330px-FrancesTiafo.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "guiomar maristany zuleta de reales": "https://thumb.wikimedia.org/wikipedia/commons/thumb/d/de/Maristany_Zuleta_de_Reales_BIA21_%2821%29_%2851379103429%29.jpg/330px-Maristany_Zuleta_de_Reales_BIA21_%2821%29_%2851379103429%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "ilya ivashka": "https://thumb.wikimedia.org/wikipedia/commons/thumb/3/34/Ivashka_MCM22_%281%29_%2852036455488%29.jpg/330px-Ivashka_MCM22_%281%29_%2852036455488%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "jaume munar": "https://thumb.wikimedia.org/wikipedia/commons/thumb/9/97/Munar_RG19_%2828%29_%2848199153407%29.jpg/330px-Munar_RG19_%2828%29_%2848199153407%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "juan pablo varillas": "https://thumb.wikimedia.org/wikipedia/commons/thumb/d/dd/Varillas_WMQ22_%2810%29.jpg/330px-Varillas_WMQ22_%2810%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "karen khachanov": "https://thumb.wikimedia.org/wikipedia/commons/thumb/d/df/Karen_Khachanov_%282024_DC_Open%29_02.jpg/330px-Karen_Khachanov_%282024_DC_Open%29_02.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "leolia jeanjean": "https://thumb.wikimedia.org/wikipedia/commons/thumb/d/d8/Jeanjean_WMQ23_%2853061887794%29.jpg/330px-Jeanjean_WMQ23_%2853061887794%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "mateus alves": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Mateus_Alves_%282023_DC_Open%29_04_%28cropped%29.jpg/330px-Mateus_Alves_%282023_DC_Open%29_04_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "max alcala gurri": "https://thumb.wikimedia.org/wikipedia/commons/thumb/f/f0/Max_Alcal%C3%A1_Gurri_%282026_US_Open%29_03_%28cropped%29.jpg/330px-Max_Alcal%C3%A1_Gurri_%282026_US_Open%29_03_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "niels vink": "https://thumb.wikimedia.org/wikipedia/commons/thumb/4/49/Niels_Vink_Paris_2024.jpg/330px-Niels_Vink_Paris_2024.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "noma noha akugue": "https://thumb.wikimedia.org/wikipedia/commons/thumb/7/74/Noha_Akugue_RGQ23.jpg/330px-Noha_Akugue_RGQ23.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "pavel kotov": "https://thumb.wikimedia.org/wikipedia/commons/thumb/5/59/Kotov_RGQ23_%2852942982357%29.jpg/330px-Kotov_RGQ23_%2852942982357%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "sam schroder": "https://thumb.wikimedia.org/wikipedia/commons/thumb/4/47/Sam_Schr%C3%B6der_Paris_2024.jpg/330px-Sam_Schr%C3%B6der_Paris_2024.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "thiago seyboth wild": "https://thumb.wikimedia.org/wikipedia/commons/thumb/7/75/Thiago_Seyboth_Wild_-_2024_Olympics_%28cropped%29.jpg/330px-Thiago_Seyboth_Wild_-_2024_Olympics_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "tokito oda": "https://thumb.wikimedia.org/wikipedia/commons/thumb/d/d5/All-Japan-Tennis-Championships-100th-Day8---2025-10-12_06704.jpg/330px-All-Japan-Tennis-Championships-100th-Day8---2025-10-12_06704.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail",
-  "yui kamiji": "https://thumb.wikimedia.org/wikipedia/commons/thumb/7/7b/Swiss_Open_Geneva_-_20140712_-_Semi_final_Women_-_Y._Kamiji_vs_K._Montjane_83.jpg/330px-Swiss_Open_Geneva_-_20140712_-_Semi_final_Women_-_Y._Kamiji_vs_K._Montjane_83.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail"
+  "alevtina ibragimova": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Alevtina_Ibragimova_%282026_US_Open%29_02_%28cropped%29.jpg/330px-Alevtina_Ibragimova_%282026_US_Open%29_02_%28cropped%29.jpg",
+  "alexander zverev": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Alexander_Zverev.jpg/330px-Alexander_Zverev.jpg",
+  "alfie hewett": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Alfie_Hewett_%2835580720080%29.jpg/330px-Alfie_Hewett_%2835580720080%29.jpg",
+  "anna blinkova": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Day_6_-_Transilvania_Open_Anna_Blinkova_%28cropped%29.jpg/330px-Day_6_-_Transilvania_Open_Anna_Blinkova_%28cropped%29.jpg",
+  "anna pushkareva": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Anna_Pushkareva_25May24_0306.jpg/330px-Anna_Pushkareva_25May24_0306.jpg",
+  "aryna sabalenka": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Aryna_Sabalenka_Miami_Open_Final.jpg/330px-Aryna_Sabalenka_Miami_Open_Final.jpg",
+  "ben shelton": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Ben_Shelton%2C_media_conference%2C_Swiss_Indoors_Basel_2025_%28cropped%29.jpg/330px-Ben_Shelton%2C_media_conference%2C_Swiss_Indoors_Basel_2025_%28cropped%29.jpg",
+  "claire liu": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Claire_Liu_%282026_US_Open%29_01_%28cropped%29.jpg/330px-Claire_Liu_%282026_US_Open%29_01_%28cropped%29.jpg",
+  "diede de groot": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/25th_Laureus_World_Sports_Awards_-_Red_Carpet_-_Diede_de_Groot_-_240422_184135_%28cropped%29.jpg/330px-25th_Laureus_World_Sports_Awards_-_Red_Carpet_-_Diede_de_Groot_-_240422_184135_%28cropped%29.jpg",
+  "elena rybakina": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Elena_Rybakina_%282025_DC_Open%29_11_%28cropped%29.jpg/330px-Elena_Rybakina_%282025_DC_Open%29_11_%28cropped%29.jpg",
+  "elsa jacquemot": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Elsa_Jacquemot_%282023_US_Open%29_09_%28cropped%29.jpg/330px-Elsa_Jacquemot_%282023_US_Open%29_09_%28cropped%29.jpg",
+  "facundo diaz acosta": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Road_2_Australia_-_Navone_V_Diaz_Acosta_semifinal_-_BugWarp_139_%28cropped%29.jpg/330px-Road_2_Australia_-_Navone_V_Diaz_Acosta_semifinal_-_BugWarp_139_%28cropped%29.jpg",
+  "fiona ferro": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Ferro_RG19_%2810%29_%2848199369072%29.jpg/330px-Ferro_RG19_%2810%29_%2848199369072%29.jpg",
+  "frances tiafoe": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/FrancesTiafo.jpg/330px-FrancesTiafo.jpg",
+  "guiomar maristany zuleta de reales": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Maristany_Zuleta_de_Reales_BIA21_%2821%29_%2851379103429%29.jpg/330px-Maristany_Zuleta_de_Reales_BIA21_%2821%29_%2851379103429%29.jpg",
+  "ilya ivashka": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Ivashka_MCM22_%281%29_%2852036455488%29.jpg/330px-Ivashka_MCM22_%281%29_%2852036455488%29.jpg",
+  "jaume munar": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Munar_RG19_%2828%29_%2848199153407%29.jpg/330px-Munar_RG19_%2828%29_%2848199153407%29.jpg",
+  "juan pablo varillas": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Varillas_WMQ22_%2810%29.jpg/330px-Varillas_WMQ22_%2810%29.jpg",
+  "karen khachanov": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Karen_Khachanov_%282024_DC_Open%29_02.jpg/330px-Karen_Khachanov_%282024_DC_Open%29_02.jpg",
+  "leolia jeanjean": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Jeanjean_WMQ23_%2853061887794%29.jpg/330px-Jeanjean_WMQ23_%2853061887794%29.jpg",
+  "mateus alves": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Mateus_Alves_%282023_DC_Open%29_04_%28cropped%29.jpg/330px-Mateus_Alves_%282023_DC_Open%29_04_%28cropped%29.jpg",
+  "max alcala gurri": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Max_Alcal%C3%A1_Gurri_%282026_US_Open%29_03_%28cropped%29.jpg/330px-Max_Alcal%C3%A1_Gurri_%282026_US_Open%29_03_%28cropped%29.jpg",
+  "niels vink": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Niels_Vink_Paris_2024.jpg/330px-Niels_Vink_Paris_2024.jpg",
+  "noma noha akugue": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Noha_Akugue_RGQ23.jpg/330px-Noha_Akugue_RGQ23.jpg",
+  "pavel kotov": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Kotov_RGQ23_%2852942982357%29.jpg/330px-Kotov_RGQ23_%2852942982357%29.jpg",
+  "sam schroder": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Sam_Schr%C3%B6der_Paris_2024.jpg/330px-Sam_Schr%C3%B6der_Paris_2024.jpg",
+  "thiago seyboth wild": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Thiago_Seyboth_Wild_-_2024_Olympics_%28cropped%29.jpg/330px-Thiago_Seyboth_Wild_-_2024_Olympics_%28cropped%29.jpg",
+  "tokito oda": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/All-Japan-Tennis-Championships-100th-Day8---2025-10-12_06704.jpg/330px-All-Japan-Tennis-Championships-100th-Day8---2025-10-12_06704.jpg",
+  "yui kamiji": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Swiss_Open_Geneva_-_20140712_-_Semi_final_Women_-_Y._Kamiji_vs_K._Montjane_83.jpg/330px-Swiss_Open_Geneva_-_20140712_-_Semi_final_Women_-_Y._Kamiji_vs_K._Montjane_83.jpg"
 });
+
+// V3.5 runtime repair: premium-shell-v1.js uses querySelector where querySelectorAll is required.
+// Keep the shell alive until its source is cleaned up, and independently hydrate every portrait.
+(()=>{
+  'use strict';
+  if(window.__TEP_RUNTIME_REPAIR_V35)return;
+  window.__TEP_RUNTIME_REPAIR_V35=true;
+
+  if(typeof Element!=='undefined'&&!Element.prototype.forEach){
+    Object.defineProperty(Element.prototype,'forEach',{configurable:true,value:function(cb,thisArg){cb.call(thisArg,this,0,[this])}});
+  }
+  if(typeof Element!=='undefined'&&!Element.prototype.filter){
+    Object.defineProperty(Element.prototype,'filter',{configurable:true,value:function(cb,thisArg){return cb.call(thisArg,this,0,[this])?[this]:[]}});
+  }
+
+  const resolved=new Map();
+  const cleanName=v=>String(v||'').trim().replace(/\s+/g,' ');
+  const keyOf=v=>cleanName(v).toLowerCase();
+  const normalizeSrc=src=>String(src||'').replace('https://thumb.wikimedia.org/','https://upload.wikimedia.org/');
+
+  async function resolvePortrait(name){
+    const key=keyOf(name);
+    if(!key)return'';
+    const catalog=normalizeSrc(window.__TEP_PLAYER_PHOTOS__?.[key]);
+    if(catalog)return catalog;
+    if(resolved.has(key))return resolved.get(key);
+    const job=(async()=>{
+      try{
+        const title=encodeURIComponent(cleanName(name).replace(/\s+/g,'_'));
+        const r=await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${title}`,{cache:'force-cache',headers:{Accept:'application/json'}});
+        if(r.ok){
+          const j=await r.json();
+          const src=normalizeSrc(j?.thumbnail?.source||j?.originalimage?.source||'');
+          if(src)return src;
+        }
+      }catch{}
+      try{
+        const q=encodeURIComponent(cleanName(name));
+        const r=await fetch(`https://www.wikidata.org/w/api.php?action=wbsearchentities&search=${q}&language=en&uselang=en&limit=6&format=json&origin=*`,{cache:'force-cache'});
+        if(r.ok){
+          const j=await r.json();
+          const hit=(j?.search||[]).find(x=>/tennis/i.test(String(x?.description||'')));
+          if(hit?.id){
+            const e=await fetch(`https://www.wikidata.org/w/api.php?action=wbgetentities&ids=${encodeURIComponent(hit.id)}&props=claims&format=json&origin=*`,{cache:'force-cache'});
+            if(e.ok){
+              const ej=await e.json();
+              const file=ej?.entities?.[hit.id]?.claims?.P18?.[0]?.mainsnak?.datavalue?.value;
+              if(file)return`https://commons.wikimedia.org/wiki/Special:FilePath/${encodeURIComponent(String(file).replace(/ /g,'_'))}?width=900`;
+            }
+          }
+        }
+      }catch{}
+      return'';
+    })();
+    resolved.set(key,job);
+    const out=await job;
+    resolved.set(key,Promise.resolve(out));
+    return out;
+  }
+
+  function reveal(el,img,src){
+    if(!el||!img||!src)return;
+    const finalSrc=normalizeSrc(src);
+    if(img.dataset.v35Src===finalSrc&&el.classList.contains('has-photo'))return;
+    img.dataset.v35Src=finalSrc;
+    img.decoding='async';
+    if(el.classList.contains('hero')){img.loading='eager';img.fetchPriority='high'}
+    img.onload=()=>{if(img.naturalWidth>40&&img.naturalHeight>40)el.classList.add('has-photo')};
+    img.onerror=()=>{el.classList.remove('has-photo')};
+    if(img.getAttribute('src')!==finalSrc)img.src=finalSrc;
+    if(img.complete&&img.naturalWidth>40&&img.naturalHeight>40)el.classList.add('has-photo');
+  }
+
+  async function hydrateOne(el){
+    if(!(el instanceof Element)||!el.matches('[data-photo]'))return;
+    const img=el.querySelector('img');
+    if(!img)return;
+    const name=el.getAttribute('data-photo')||img.alt||'';
+    const catalog=normalizeSrc(window.__TEP_PLAYER_PHOTOS__?.[keyOf(name)]||'');
+    if(catalog){reveal(el,img,catalog);return}
+    if(el.dataset.v35Lookup==='1')return;
+    el.dataset.v35Lookup='1';
+    const src=await resolvePortrait(name);
+    if(src)reveal(el,img,src);
+  }
+
+  function hydrateAll(root=document){
+    if(root instanceof Element&&root.matches('[data-photo]'))void hydrateOne(root);
+    root.querySelectorAll?.('[data-photo]').forEach(el=>void hydrateOne(el));
+  }
+
+  function syncNav(route){
+    document.querySelectorAll('#tpRoot .tp-nav [data-route]').forEach(b=>b.classList.toggle('on',b.dataset.route===route));
+  }
+
+  function bootRepair(){
+    hydrateAll(document);
+    const obs=new MutationObserver(records=>{
+      for(const rec of records){for(const n of rec.addedNodes){if(n.nodeType===1)hydrateAll(n)}}
+    });
+    obs.observe(document.documentElement,{subtree:true,childList:true});
+    document.addEventListener('pointerdown',e=>{
+      const b=e.target.closest?.('#tpRoot .tp-nav [data-route]');
+      if(b)syncNav(b.dataset.route);
+    },true);
+    document.addEventListener('click',e=>{
+      const b=e.target.closest?.('#tpRoot .tp-nav [data-route]');
+      if(b)syncNav(b.dataset.route);
+    },true);
+  }
+
+  if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',()=>setTimeout(bootRepair,0),{once:true});
+  else setTimeout(bootRepair,0);
+})();
